b83750bf6a2a3b2ab9b6c4e5f138f2a27e797f50
sayedkhan196 <sayedkhan196>
2025-06-17 00:10:42 +0600

    added delete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+const { ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -51,6 +52,13 @@ async function run() {
         res.send(result);
         
     });
+
+     app.get("/service/:id", async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await serviceCollention.findOne(query);
+        res.send(result)
+    })
 
     // DELETE a service
  app.delete('/service/:id', async (req, res) => {
